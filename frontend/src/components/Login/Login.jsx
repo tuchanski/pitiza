3a978f91cd5b3c163559576c2d76897dcc15ec75
Tuchanski <gui.rocha2004@gmail.com>
2025-09-12 14:58:09 -0300

    Add login screen

---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./Login.module.css";
+import pitizaLogo from "../../assets/pitiza.svg";
+import showingPizza from "../../assets/showing-pizza.png";
+
+function Login() {
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["login-container"]}>
+        <img src={pitizaLogo} alt="Pitiza" id={styles["pitiza-logo"]} />
+        <form className={styles["login-form"]}>
+          <div className={styles["form-group"]}>
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" name="username" required />
+          </div>
+          <div className={styles["form-group"]}>
+            <label htmlFor="password" id={styles.password}>
+              Password:
+            </label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <button type="submit">Sign In</button>
+        </form>
+      </div>
+      <img src={showingPizza} alt="" className={styles.showingPizza} />
+    </div>
+  );
+}
+
+export default Login;
