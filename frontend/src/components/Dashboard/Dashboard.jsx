05177d0438d1fc492d5092651e2e9caed2b5732b
Tuchanski <gui.rocha2004@gmail.com>
2025-09-13 09:06:28 -0300

    Update Navbar and add options to dashboard

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,33 @@
 import React from "react";
+import { useState } from "react";
 import styles from "./Dashboard.module.css";
 import Navbar from "./Navbar/Navbar.jsx";
 
 function Dashboard() {
+  const [user, setUser] = useState("");
+
+  function fetchUserData() {
+    // Simulate fetching user data from an API
+    setUser({ name: "John Doe" });
+  }
+
+  React.useEffect(() => {
+    fetchUserData();
+  }, []);
+
   return (
     <div className={styles.dashboard}>
       <Navbar />
-      <h1>Welcome to the Dashboard</h1>
+
+      <div className={styles.container}>
+        <div className={styles["container-options"]}>
+          <p>Welcome back, {user.name} 😊</p>
+          <div className={styles["container-options-buttons"]}>
+            <button className={styles["btn-create"]}>Create Order</button>
+            <button className={styles["btn-search"]}>Search Order</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/components/Dashboard/Navbar/Navbar.jsx
+++ frontend/src/components/Dashboard/Navbar/Navbar.jsx
@@ -1,11 +1,21 @@
 import styles from "./Navbar.module.css";
+import { useState } from "react";
 import React from "react";
 import pitizaLogo from "../../../assets/pitiza.svg";
 
 function Navbar() {
+  const [restaurant, setRestaurant] = useState("");
+
+  React.useEffect(() => {
+    // Simulate fetching restaurant data from an API
+    setRestaurant({ name: "Your Restaurant" });
+  }, []);
+
   return (
     <div className={styles.navbar}>
-      <h2>Pitiza</h2>
+      <h2>
+        {restaurant.name} <span id={styles.powered}>Pitiza™</span>
+      </h2>
       <button id={styles["log-out"]}>Log Out</button>
     </div>
   );
