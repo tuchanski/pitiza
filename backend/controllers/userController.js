a817806bff9c23317a6f239f5a806f5ad879e0bd
Tuchanski <gui.rocha2004@gmail.com>
2025-09-16 15:31:43 -0300

    Add create user endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,7 @@ export const getAllUsers = async (req, res) => {
     res.json(rows);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: "Erro ao buscar usuários" });
+    res.status(500).json({ error: "Internal server error." });
   }
 };
 
@@ -16,9 +16,39 @@ export const getUserById = async (req, res) => {
     const [rows] = await db.query(
       `SELECT * FROM user WHERE id_user = ${userId}`
     );
-    res.json(rows);
+    if (rows.length !== 0) {
+      res.json(rows);
+    } else {
+      res.status(500).json({ error: "User not found." });
+    }
   } catch (err) {
     console.log(err);
-    res.status(500).json({ error: "User not found " });
+    res.status(500).json({ error: "Internal server error." });
+  }
+};
+
+export const createUser = async (req, res) => {
+  const values = [
+    req.body.username,
+    req.body.name,
+    req.body.password,
+    req.body.restaurant_name,
+  ];
+
+  const sql = `INSERT INTO user (username, name, password, restaurant_name) VALUES (?, ?, ?, ?)`;
+
+  try {
+    const result = await db.query(sql, values);
+
+    if (result) {
+      res.status(200).json({ message: "User created successfully " });
+    }
+  } catch (err) {
+    if (err.code === "ER_DUP_ENTRY") {
+      return res.status(409).json({ error: "Username already registered." });
+    }
+
+    console.error(err);
+    res.status(500).json({ error: "Internal server error." });
   }
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { getAllUsers, getUserById } from "../controllers/userController.js";
+import {
+  createUser,
+  getAllUsers,
+  getUserById,
+} from "../controllers/userController.js";
 
-export const router = express.Router();
+const router = express.Router();
 
 router.get("/users", getAllUsers);
 router.get("/users/:id", getUserById);
+
+router.post("/users", createUser);
 
 export default router;
